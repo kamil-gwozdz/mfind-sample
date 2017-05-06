a783c55ba6faa73036902fc3a71f88f965c65892
Kamil Gwóźdź <kamil@MacBook-Pro-Kamil.local>
2017-05-06 22:01:58 +0200

    keys reset

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '32131876716-k5m51fmn6bktpu63og2hktih8i3up745.apps.googleusercontent.com', 'UhwyXeu-i2PvU6jEg9TyTADo', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
